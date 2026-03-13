62c2f47e4a892e8e472ec552ec1472fe0cf7239b
arjun <3907@oasys.com>
2026-03-13 22:04:35 +0530

    update

---
+++ app/src/app/personnel/employee-self-service/promotion/edit/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function EditPromotionPage() {
+  return (
+    <div className="p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-xl font-semibold text-gray-700 mb-1">Edit Promotion</h1>
+      <p className="text-sm text-gray-500 mb-4">
+        <span className="text-gray-400">Home / Personnel / Employee Self Service / </span>Edit Promotion
+      </p>
+    </div>
+  );
+}
